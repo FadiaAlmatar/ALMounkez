d25f9b90125b1dbaa57a4de2d41adb0e27c11581
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-08-30 11:13:44 +0300

    fix

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -1,6 +1,7 @@
 <x-layouts.app>
     <section class="section" style="height: 100%;width:100%;margin:auto;">
         <div class="container">
+            {{-- start section friends list with unread messages --}}
         <div style="text-align:center;width:20%;border-color: red;border-style:solid;float:left;display:inline-block;height: 100%">
             @if($friends <> null)
                <br><span>FRIENDS</span><br>
@@ -32,30 +33,30 @@
           @endforeach
           @endif
         </div>
+        {{-- end section friends list with unread messages --}}
+        {{-- start section chat --}}
         <div style="width:75%;float:right;">
           <h5>Chat with {{$friend_name}}</h5>
+          {{-- start form send message --}}
           <form action="{{ route('messages.store') }}" method="POST" >
             @csrf
             <input name="friend_id" value ={{$friend_id}} hidden>
-          <textarea style="width:50%"class=" @error('message_content')is-danger @enderror" name="message_content" placeholder="write message here...">{{ old('message_content') }}</textarea>
-          <button class="button is-dark" style="color: #eb640a;align:center" >send</button>
-        </form>
+            <textarea style="width:50%"class=" @error('message_content')is-danger @enderror" name="message_content" placeholder="write message here...">{{ old('message_content') }}</textarea>
+            <button class="button is-dark" style="color: #eb640a;align:center" >send</button>
+          </form>
+          {{--end form send message   --}}
+          {{-- start show messages --}}
           <div style="overflow:auto;height:500px">
               @foreach($messages as $message)
               <div class="card">
                   <div class="card-content">
-                     {{-- <h6><u style="text-transform: capitalize">{{$message->user->name}}</u>&nbsp; {{$message->created_at}}</h6> --}}
-                     {{-- {{$sender = User::find($message->user_id)->name}} --}}
                     <input value={{$sender = $users->where('id',$message->user_id)->first()}} hidden/>
                      <h5 style="font-weight: bold">{{$sender->name}} </h5><br>
-                         {{-- to {{$message->friend_id}} --}}
                      <span>{{$message->message_content}}</span><br>
                      @if(Auth::User()->id == $message->user_id)
                        @if($message->seen == 1)
-                         {{-- <span style="color: red">seen</span> --}}
                          <i class="fa fa-check-double fa-xs" aria-hidden="true"></i>
                         @else
-                        {{-- <span style="color: red">not seen</span> --}}
                         <i class="fa fa-check fa-xs" aria-hidden="true" ></i>
                         @endif
                      @endif
@@ -64,7 +65,9 @@
               </div>
               @endforeach
           </div>
+          {{-- end show messages --}}
         </div>
+        {{-- end section chat --}}
         </div>
     </section>
 </x-layouts.app>
