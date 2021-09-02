a9075ce5c9c729e61ef7891f42f691b390cf15a2
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-02 13:10:42 +0300

    date

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -78,7 +78,7 @@ class MessageController extends Controller
         ])->orwhere([
             ['friend_id', Auth::User()->id],
             ['user_id', $friend->id],])->orderBy('created_at','DESC')->get()->groupBy(function($date) {
-                    return Carbon::parse($date->created_at)->format('Y-m-d'); });
+                return Carbon::parse($date->created_at)->format('Y-m-d'); });
         $friend_name = User::find($friend->id);
         $users = User::all();
         foreach($messages as $message){
