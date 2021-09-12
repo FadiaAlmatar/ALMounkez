38db3074e2be8efb2bacceb68321a649e3165651
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-12 15:01:36 +0300

    fix some issue

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,46 +1,71 @@
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="#" style="color: #eb640a">Welcome @auth {{Auth::User()->name}} @endauth</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <a class="navbar-brand" href="#" style="color: #eb640a">{{__('Welcome')}} @auth {{Auth::User()->name}} @endauth</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                 <li class="nav-item ">
+                     <a class="nav-link active" aria-current="page" href="{{route('posts.index')}}" style="color: #eb640a">{{__('posts')}}</a>
+                 </li>
+                 @auth
+                 <li class="nav-item ">
+                     <a class="nav-link active" aria-current="page" href="{{route('posts.create')}}" style="color: #eb640a">{{__('Create post')}}</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link active" aria-current="page"   href="{{route('messages.create')}}"  style="color: #eb640a">
+                       {{__('chat')}}
+                     </a>
+                 </li> @endauth
+                 <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: orange">
+                      {{ __('lang') }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="/locale/ar" style="color: orange">{{ __('Ar') }}</a></li>
+                      <li><a class="dropdown-item" href="/locale/en" style="color: orange">{{ __('En') }}</a></li>
+                    </ul>
+                 </li>
+                  <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: orange">
+                      {{ __('account') }}
+                     </a>
+                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      @guest<li><a class="dropdown-item" href="{{ asset('login') }}" style="color: orange">{{ __('Login') }}</a></li>
+                      <li><a class="dropdown-item" href="{{ asset('register') }}" style="color: orange">{{ __('register') }}</a></li>@endguest
+                      <li><hr class="dropdown-divider"></li>
+                      @auth
+                      <li><form action="logout" method="post">
+                        @csrf
+                        <input type="submit" class="button is-light" value="{{ __('Logout') }}">
+                      </form></li>@endauth
+                    </ul>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input id="search"class="form-control me-2" type="search" placeholder="{{__('search')}}" aria-label="Search" style="border-color:#eb640a">
+                  <button  type="submit" class="button is-dark" style="color: #eb640a;align:center">{{__('Search')}}</button>
+                </form>
+              </div>
+            </div>
+</nav>
           {{-- <li class="nav-item ">
             <a class="nav-link active" aria-current="page" href="/" style="color: #eb640a">Home</a>
           </li> --}}
           {{-- <li class="nav-item ">
             <a class="nav-link active" aria-current="page" href="{{route('messages.create')}}" style="color: #eb640a">Chat</a>
           </li> --}}
-          <li class="nav-item ">
-            <a class="nav-link active" aria-current="page" href="{{route('posts.index')}}" style="color: #eb640a">posts</a>
-          </li>
+
           {{-- <li class="nav-item ">
             <a class="nav-link active" aria-current="page" href="{{route('comments.index')}}" style="color: #eb640a">comments</a>
           </li> --}}
-          @auth
-          <li class="nav-item ">
-            <a class="nav-link active" aria-current="page" href="{{route('posts.create')}}" style="color: #eb640a">Create post</a>
-          </li>
 
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page"   href="{{route('messages.create')}}"  style="color: #eb640a">
-              chat
-            </a>
-            </li> @endauth
             {{-- <li class="nav-item">
                 <a class="nav-link active" href="{{ route('locale.toggle', App::getLocale() == 'ar' ? 'en': 'ar') }}" style="color:#eb640a">{{ App::getLocale() == 'ar' ? 'EN' : 'AR' }}</a>
             </li> --}}
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: orange">
-                  {{ __('lang') }}
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="/locale/ar" style="color: orange">{{ __('Ar') }}</a></li>
-                  <li><a class="dropdown-item" href="/locale/en" style="color: orange">{{ __('En') }}</a></li>
-                </ul>
-              </li>
+
 
           {{-- <li class="nav-item"> --}}
             {{-- <a class="nav-link active" href="{{route('home')}}" style="color: #eb640a">Posts</a> --}}
@@ -64,21 +89,7 @@
             {{-- </ul> --}}
             {{-- </li> --}}
 
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: orange">
-              {{ __('account') }}
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              @guest<li><a class="dropdown-item" href="{{ asset('login') }}" style="color: orange">{{ __('Login') }}</a></li>
-              <li><a class="dropdown-item" href="{{ asset('register') }}" style="color: orange">{{ __('register') }}</a></li>@endguest
-              <li><hr class="dropdown-divider"></li>
-              @auth
-              <li><form action="logout" method="post">
-                @csrf
-                <input type="submit" class="button is-light" value="{{ __('Logout') }}">
-              </form></li>@endauth
-            </ul>
-          </li>
+
             {{-- @endguest --}}
               {{-- @auth
               <li><form action="{{ route('logout') }}" method="post">
@@ -86,11 +97,4 @@
                 <input type="submit" class="button is-dark" style="color: #eb640a;background:none;align:center" value="Logout">
               </form></li>
               @endauth --}}
-        </ul>
-        <form class="d-flex">
-          <input id="search"class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="border-color:#eb640a">
-          <button  type="submit" class="button is-dark" style="color: #eb640a;align:center">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+
--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -1,8 +1,7 @@
 <x-layouts.app>
-    {{-- <section class="section" style="height: 100%;width:100%;margin:auto;"> --}}
         <div class="container" style="margin-top: 5px">
-            {{-- start section friends list with unread messages --}}
-        <div style="text-align:center;width:28%;background: #e6e9ec;float:left;display:inline-block;margin-top:30px;overflow:auto">
+{{-- start section friends list with unread messages --}}
+        <div class="friends-section">
             @if($friends <> null)
                <?php $var = false ?>
                @foreach ($friends as $friend)
@@ -10,11 +9,11 @@
                   <?php $var = false ?>
                   @foreach ($unread_messages as $unread_message)
                      @if($friend->user_id == $unread_message->user_id)
-                      <a style="text-decoration: none;color:black;font-weight:bold;" href="{{route('messages.chat', $unread_message->user_id)}}">{{ App\Models\User::where(['id' => $unread_message->user_id])->pluck('name')->first() }}</a>
+                      <a class="unread-friend-name" href="{{route('messages.chat', $unread_message->user_id)}}">{{ App\Models\User::where(['id' => $unread_message->user_id])->pluck('name')->first() }}</a>
                       <?php $var = true ?>
                       @foreach ($count_unread_messages as $count_unread_message)
                         @if($unread_message->user_id == $count_unread_message->user_id)
-                           <span style="color:black;font-weight:bold">{{$count_unread_message->number}}</span><br>
+                           <span class="count-unread-message">{{$count_unread_message->number}}</span><br>
                         @else
                          @continue
                         @endif
@@ -23,7 +22,7 @@
                      @endif
                   @endforeach
                   @if($var == false)
-                    <a style="text-decoration: none;color:blue;font-weight:bold" href="{{route('messages.chat', $friend->user_id)}}">{{ App\Models\User::where(['id' => $friend->user_id])->pluck('name')->first() }}</a><br>
+                    <a class="friend-name" href="{{route('messages.chat', $friend->user_id)}}">{{ App\Models\User::where(['id' => $friend->user_id])->pluck('name')->first() }}</a><br>
                 @endif
                     <?php
                     $last_message = [];
@@ -34,24 +33,23 @@
                 @endforeach
             @endif
         </div>
-        {{-- end section friends list with unread messages --}}
-        {{-- start section chat --}}
-        <div style="width:70%;float:right;">
-          <h5 style="text-align: center">{{__('Chat with')}} {{$friend_name}}</h5>
-          {{-- start form send message --}}
+{{-- end section friends list with unread messages --}}
+{{-- start section chat --}}
+        <div class="chat-section">
+          <h5>{{__('Chat with')}} {{$friend_name}}</h5>
+{{-- start form send message --}}
           <form action="{{ route('messages.store') }}" method="POST" >
             @csrf
             <input name="friend_id" value ={{$friend_id}} hidden>
             <textarea style="width:100%"class=" @error('message_content')is-danger @enderror" name="message_content" placeholder="{{__('write message here...')}}">{{ old('message_content') }}</textarea>
-            <button class="btn btn-light" style="color: white;align:center;margin-bottom:13px" ><i class="fa fa-paper-plane fa-lg" aria-hidden="true" style="color:blue"></i></button>
+            <button class="btn btn-light chat-send-btn"><i class="fa fa-paper-plane fa-lg" aria-hidden="true"></i></button>
             <a href="{{route('messages.print', $friend_id)}}"><i class="fas fa-file-pdf fa-2x" style="color:red"></i></a>
             <div class="justify-content-center">
                 {!! $messages->links() !!}
             </div>
-            {{-- <a class="btn btn-primary" style="margin-bottom:13px" href="{{route('messages.print', $friend_id)}}">Download PDF</a> --}}
           </form>
-          {{--end form send message   --}}
-          {{-- start show messages --}}
+{{--end form send message   --}}
+{{-- start show messages --}}
           <div style="overflow:auto;height:500px;">
               @foreach($messages as $message)
                @if($message->user_id == Auth::User()->id)
@@ -59,8 +57,8 @@
                   <div class="card-content">
                     <input value={{$sender = $users->where('id',$message->user_id)->first()}} hidden/>
                      <span class="sender-name">{{$sender->name}} </span>
-                     <span style="font-size: 10px;float: right;">{{ date("Y-m-d h:i A", strtotime($message->created_at))}}</span><br><br>
-                     <div style="background: white;margin:auto">{{$message->message_content}}
+                     <span class="message-date-sender">{{ date("Y-m-d h:i A", strtotime($message->created_at))}}</span><br><br>
+                     <div class="message-content">{{$message->message_content}}
                        @if($message->seen == 1)
                          <i class="fa fa-check-double fa-xs" aria-hidden="true"></i>
                         @else
@@ -73,21 +71,19 @@
               <div class="card" style="margin-bottom: 3px">
                 <div class="card-content">
                   <input value={{$sender = $users->where('id',$message->user_id)->first()}} hidden/>
-                   <span style="font-size: 10px;float: left;">{{ date("Y-m-d h:i A", strtotime($message->created_at))}}</span>
+                   <span class="message-date-receiver">{{ date("Y-m-d h:i A", strtotime($message->created_at))}}</span>
                    <span class="receiver-name">{{$sender->name}} </span><br><br>
-                   <div style="background: white;margin:auto">{{$message->message_content}}
+                   <div class="message-content">{{$message->message_content}}
                 </div>
                 </div>
             </div>
               @endif
               @endforeach
           </div>
-          {{-- end show messages --}}
+{{-- end show messages --}}
         </div>
-        {{-- end section chat --}}
-         {{-- Pagination --}}
-
+{{-- end section chat --}}
         </div>
-    {{-- </section> --}}
-
+{{-- </section> --}}
 </x-layouts.app>
+            {{-- <a class="btn btn-primary" style="margin-bottom:13px" href="{{route('messages.print', $friend_id)}}">Download PDF</a> --}}
