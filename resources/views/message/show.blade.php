c3770fe8428e272299b67a32868ae0e361947096
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-16 11:26:47 +0300

    group

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -93,9 +93,7 @@
           <form action="{{ route('messages.store') }}" method="POST" >
             @csrf
             <input name="friend_id" value ={{$friend_id}} hidden>
-            {{-- @if($request->group_id <> 0) --}}
             <input name="group_id" value ={{$group_id}} hidden>
-            {{-- @endif --}}
             <textarea style="width:100%"class=" @error('message_content')is-danger @enderror" name="message_content" placeholder="{{__('write message here...')}}">{{ old('message_content') }}</textarea>
             <button class="btn btn-light chat-send-btn"><i class="fa fa-paper-plane fa-lg" aria-hidden="true"></i></button>
             <a href="{{route('messages.print', $friend_id)}}"><i class="fas fa-file-pdf fa-2x" style="color:red"></i></a>
@@ -137,11 +135,9 @@
               @endforeach
           </div>
 {{-- end show messages --}}
-
         </div>
 {{-- end section chat --}}
         </div>
-{{-- </section> --}}
 </x-layouts.app>
 
 
