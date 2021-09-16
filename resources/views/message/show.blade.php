c68961891219ffe3d958fe2389009359fb150d0e
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-16 12:49:55 +0300

    add new subscribes

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -96,7 +96,7 @@
             <textarea style="width:100%"class=" @error('message_content')is-danger @enderror" name="message_content" placeholder="{{__('write message here...')}}">{{ old('message_content') }}</textarea>
             <button class="btn btn-light chat-send-btn"><i class="fa fa-paper-plane fa-lg" aria-hidden="true"></i></button>
             <a href="{{route('messages.print', $friend_id)}}"><i class="fas fa-file-pdf fa-2x" style="color:red"></i>
-            <button class="btn btn-light chat-send-btn"><i class="fas fa-plus" aria-hidden="true"></i></button></a>
+            @if($group_id <> 0)<button class="btn btn-light chat-send-btn"><i class="fas fa-plus" aria-hidden="true"></i></button>@endif</a>
 {{-- pagination --}}
             {{-- <div class="d-flex justify-content-center">
                 {!! $messages->links() !!}
