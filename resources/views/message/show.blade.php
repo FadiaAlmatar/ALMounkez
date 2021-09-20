f34b3d78f151e76778bdfb03fca4d454f2bb98a3
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-20 09:58:35 +0300

    fix

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -113,7 +113,7 @@
           <form action="{{route('groups.destroy',$group_id)}}" method="POST" style="display:inline">
              @csrf
              @method('DELETE')
-             <button class="btn btn-light chat-send-btn" ><i style="color:red"class="fa fa-trash" aria-hidden="true"></i></button>
+             <button class="btn btn-light chat-send-btn"><i style="color:red"class="fa fa-trash" aria-hidden="true"></i></button>
           </form>
           {{-- end delete group --}}
           {{-- start delete conversation --}}
