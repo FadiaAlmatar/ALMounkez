f6e954802963f60efe8bc65e28d89da466966ce7
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-20 12:04:57 +0300

    make order model

---
+++ database/migrations/2021_09_20_104246_create_orders_table.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');//الاسم
+            $table->string('lastname');//النسبة
+            $table->string('father_name');//اسم الاب
+            $table->string('grandfather_name');//اسم الجد
+            $table->string('mother_name');//اسم الام
+            $table->string('gender');//الجنس
+            $table->string('english_firstname');//االاسم بالانكليزي
+            $table->string('english_lastname');//النسبة بالانكليزي
+            $table->string('english_father_name');//اسم الاب انكليزي
+            $table->string('english_mother_name');//اسم الام انكليزي
+            $table->string('Nationality');//الجنسية
+            $table->string('Marital_status');//الوضع العائلي
+            $table->string('place_of_birth');//مكان الولادة
+            $table->date('date_of_birth');//تاريخ الولادة
+            $table->long('national_id');// الرقم الوطني
+            $table->string('civil_registry_secretariat');//أمانة السجل المدني
+            $table->integer('personal_identification_number');//رقم الهوية الشخصية
+            $table->date('Identity_grant_date');//تاريخ منح الهوية
+            $table->string('constraint');//محل ورقم القيد
+            $table->string('military');//خدمة العلم
+            $table->int('public_record_number');//رقم السجل العام
+            $table->string('Health_status');//الوضع الصحي
+            $table->string('Affiliation_country');//المحافظة التي يرغب النتساب بها
+            $table->string('address');//العنوان
+            $table->string('house_phone');//هاتف منزل
+            $table->string('work_phone');//هاتف مكتب
+            $table->int('mobile');//موبايل
+            $table->string('email');//بريد الكتورني
+            $table->string('fax');//فاكس
+            $table->string('site');//موقع الكتروني
+            $table->string('work');//العمل في الدولة
+            $table->string('side_work');//جهة العمل
+            $table->string('insurance_number');//الرقم التأميني
+            $table->string('qualification');//المؤهل
+            $table->string('university');//الجامعه
+            $table->string('country');//البلد
+            $table->date('graduation_year');//سنة التخرج
+            $table->string('graduation_rate');//معدل التخرج
+            $table->string('Specialization');//التخصص
+            $table->string('pay_affiliation_fee');//طريقة سداد رسم الانتساب
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
