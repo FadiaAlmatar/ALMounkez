88f01cde71db96f78e3a483c9c82b36eccf4371e
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-21 13:10:03 +0300

    order

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -331,7 +331,7 @@
           </div>
           <button type="submit" class="btn btn-primary">{{__('Send')}}</button>
       </form>
-      <ul>
+      <ul style="width:70%;margin:auto;list-style-type:disc;">
           <li>{{__('Name field is required')}}</li>
           <li>{{__('Nickname field is required')}}</li>
           <li>{{__('Name/English field is required-Please write the name as it is in the passport, if any')}}</li>
@@ -340,6 +340,17 @@
           <li>{{__('FatherName/English field is required-Please write the name as it is in the passport, if any')}}</li>
           <li>{{__('MotherName/English field is required-Please write the name as it is in the passport, if any')}}</li>
           <li>{{__('GrandFathername/Father field is required')}}</li>
+          <li>{{__('Mothername field is required')}}</li>
+          <li>{{__('Place of birth field is required')}}</li>
+          <li>{{__('Date of birth is required')}}</li>
+          <li>{{__('National ID field is required')}}</li>
+          <li>{{__('Civil Registry field is required')}}</li>
+          <li>{{__('Personal Identification Number field is required')}}</li>
+          <li>{{__('Identity Grant Date field is required')}}</li>
+          <li>{{__('Constraint field is required')}}</li>
+          <li>{{__('country you wish to join field is required')}}</li>
+          <li>{{__('Address field is required')}}</li>
+          <li>{{__('Desire to display data on the site field is required')}}</li>
       </ul>
 
 </x-layouts.app>
