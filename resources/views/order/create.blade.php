90a9164b27a86e94e1aab9e077c0af48ef2e96dc
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-09-21 09:15:50 +0300

    order

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -6,29 +6,31 @@
           <small id="emailHelp" class="form-text text-muted">{{__('Username is the name used to enter the site to submit the affiliation request,It must not contain spaces')}}<br>
             {{__('Username must be in English If the number of letters is between 3 and 20 letters')}}</small>
         </div>
-        <div class="form-group">
+        <br><div class="form-group">
           <label for="exampleInputPassword1">{{__('Password')}}</label>
           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="{{__('password')}}">
           <small id="emailHelp" class="form-text text-muted">{{__('password must be at least 8 characters (and a maximum of 20 characters), and contain at least one number and one character.')}}</small>
         </div>
-        <div class="form-group">
+       <br> <div class="form-group">
             <label for="exampleInputPassword1">{{__('Confirm Password')}}</label>
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="{{__('confirm password')}}">
             <small id="emailHelp" class="form-text text-muted">{{__('password must be at least 8 characters (and a maximum of 20 characters), and contain at least one number and one character.')}}</small>
           </div>
-          <div class="form-group">
+          <br><div class="form-group">
             <label for="exampleInputPassword1">{{__('Name')}}</label>
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="{{__('name')}}">
             <small id="emailHelp" class="form-text text-muted">{{__('name is what appears on the site after logging in')}}</small>
           </div>
-          <div class="form-group">
+          <br><div class="form-group">
             <label for="exampleInputPassword1">{{__('Nickname')}}</label>
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="{{__('nickname')}}">
             <small id="emailHelp" class="form-text text-muted">{{__('nickname must be at least 3 characters ')}}</small>
           </div>
+          <p>{{__('')}}</p>
           <div class="form-group">
             <label for="exampleInputPassword1">{{__('E-mail')}}</label>
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="{{__('e-mail')}}">
+            <small id="emailHelp" class="form-text text-muted">{{__('')}}</small>
           </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
