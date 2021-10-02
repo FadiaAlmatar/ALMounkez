99cfb8238590c492240aac2a069d9623c0ebc2bf
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-10-02 11:15:30 +0300

    one to many between user and fullorder

--- app/Models/FullOrder.php
+++ app/Models/FullOrder.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class FullOrder extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,6 +58,10 @@ class User extends Authenticatable
     {
         return $this->hasOne(Order::class);
     }
+    public function fullorders()
+    {
+        return $this->hasMany(FullOrder::class);
+    }
 
 
 
