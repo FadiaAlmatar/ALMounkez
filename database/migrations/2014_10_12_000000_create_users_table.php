be99d1b60987c5f82892c8f206cb7833c76486f4
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-10-05 15:00:16 +0300

    local order

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId('group_id')->default(0);
+            $table->string('role')->default("user");
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/fullorder/create_local.blade.php
+++ resources/views/fullorder/create_local.blade.php
@@ -21,26 +21,28 @@
         <hr><br>
 {{-- manager only --}}
 {{-- بيان الدارة المالية --}}
+    @if(Auth::User()->role == "admin")
         <p style="font-weight: bold;">{{__('Financial Management Statement:')}}</p><hr>
-        <p style="display:inline">{{__('Mr.')}} <span style="font-weight: bold">.......</span>{{__(' is affiliated with the Syndicate with a membership number ')}}/......./<br>
+        <p style="display:inline">{{__('Mr.')}} <span style="font-weight: bold">{{Auth::User()->name}}</span>{{__(' is affiliated with the Syndicate with a membership number ')}}{{Auth::User()->id}}<br>
             {{__('We inform you that he is registered in the Syndicate in year ...... and : ')}}</p>&nbsp;
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="debt" id="financially_innocent" value="option1">
+            <input class="form-check-input" type="radio" name="debt" id="financially_innocent" value="option1" disabled>
             <label for="financially_innocent" class="form-check-label" value="financially_innocent" @if (old('debt') == "financially_innocent") {{ 'selected' }} @endif>{{__('Financially innocent')}}</label>
           </div>
           <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="debt" id="financial_liability" value="option2">
+            <input class="form-check-input" type="radio" name="debt" id="financial_liability" value="option2" disabled>
             <label for="financial_liability" class="form-check-label" value="financial_liability" @if (old('debt') == "financial_liability") {{ 'selected' }} @endif>{{__('It has a previous financial liability')}}</label>
            </div><br><br>
-         <p>{{__('Mr.: The cashier in the branch, please receive an amount and its amount ')}}/......../{{__('SYP')}}</p>
+         <p>{{__('Mr.: The cashier in the branch, please receive an amount and its amount ')}}<input style="width:150px;"type="text" class="input @error('money_debt')is-danger @enderror"id="money_debt" name="money_debt"value="{{ old('money_debt') }}"class="form-control" placeholder="{{__('Enter debt money')}}" disabled/>{{__('SYP')}}</p><br>
 {{--  بيان أمين الصندوق--}}
         <hr><br>
-        <p style="font-weight: bold;">{{__('Treasurer statement: ')}}</p><hr>
-        <p>{{__('Amount has been received')}}/......../{{__('SYP')}}</p><br><br>
+        <p style="font-weight: bold;">{{__('Treasurer statement: ')}}</p><hr><br>
+        <p>{{__('Amount has been received')}}<input style="width:150px;"type="text" class="input @error('money_order')is-danger @enderror"id="money_order" name="money_order"value="{{ old('money_order') }}"class="form-control" placeholder="{{__('Enter order money')}}" disabled/>{{__('SYP')}}</p><br><br>
 {{-- قرار رئيس مجلس الإدارة --}}
         <hr><br>
         <p style="font-weight: bold;">{{__("Chairman's decision: ")}}</p><br>
-        <textarea class="form-control" id="reasons" rows="3"></textarea><hr><br><br>
+        <textarea class="form-control" id="reasons" rows="3" disabled></textarea><hr><br><br>
+        @endif
 
     </div>
 </x-layouts.app>
