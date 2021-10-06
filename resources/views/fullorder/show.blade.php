135b6593164bb005b49661bfa97163308118520d
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-10-06 10:52:31 +0300

    fix

--- resources/views/fullorder/show.blade.php
+++ resources/views/fullorder/show.blade.php
@@ -17,27 +17,34 @@
             <tr>
                 <td style="width:24%"><input type="text" class="input"id="" name=""value="{{ Auth::User()->id}}"class="form-control" placeholder="" readonly/></p></td>
                 <td style="width:24%"><input type="text" class="input"id="" name=""value="{{$fullorder->membership_id}}"class="form-control" placeholder="" readonly/></td>
-                <td style="width:24%"><input type="text" class="input"id="" name=""value="{{$fullorder->created_at}}"class="form-control" placeholder="" readonly /></td>
+                <td style="width:24%"><input type="text" class="input"id="" name=""value="{{ date("Y-m-d h:i A", strtotime($fullorder->created_at))}}"class="form-control" placeholder="" readonly /></td>
             </tr>
         </tbody>
         </table>
         <p style="text-align:center;">{{__('Your request will be considered within a maximum period of two days. Please contact us')}}</p>
         <hr><br>
-        {{-- manager only --}}
-        <p style="font-weight: bold;">{{__('Financial Management Statement:')}}</p><hr>
-        <p style="display:inline">{{__('Mr.')}} <span style="font-weight: bold">{{Auth::User()->name}}</span>{{__(' is affiliated with the union with a membership number ')}}{{1111}}<br>
-        {{__('We inform you that he is registered in the union in year and : ')}}</p>
-        <select style="width:150px;display:inline-block"name="debt"id="debt"class="form-select form-select-sm" aria-label=".form-select-sm example">
-            <option></option>
-            <option value="financially_innocent" @if (old('debt') == "financially_innocent") {{ 'selected' }} @endif>{{__('Financially innocent')}}</option>
-            <option value="financial_liability" @if (old('debt') == "financial_liability") {{ 'selected' }} @endif>{{__('It has a previous financial liability')}}</option>
-        </select>
-        <p>{{__('Mr.: The cashier in the branch, please receive an amount and its amount')}}<input style="width:150px;"type="text" class="input @error('')is-danger @enderror"id="" name=""value="" class="form-control form-control-sm" placeholder="" />{{__('SYP')}}</p>
-        <hr><br>
-        <p style="font-weight: bold;">{{__('Treasurer statement: ')}}</p><hr>
-        <p>{{__('Amount has been received')}} {{__('SYP')}}</p>
-        <hr><br>
-        <p style="font-weight: bold;">{{__("Chairman's decision")}}</p><br><br>
-
+       {{-- بيان الدارة المالية --}}
+    @if(Auth::User()->role == "user")
+    <p style="font-weight: bold;">{{__('Financial Management Statement:')}}</p><hr>
+    <p style="display:inline">{{__('Mr.')}} <span style="font-weight: bold">{{Auth::User()->name}}</span>{{__(' is affiliated with the Syndicate with a membership number ')}}{{Auth::User()->id}}<br>
+        {{__('We inform you that he is registered in the Syndicate in year ...... and : ')}}</p>&nbsp;
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="debt" id="financially_innocent" value="option1" disabled>
+        <label for="financially_innocent" class="form-check-label" value="financially_innocent" @if (old('debt') == "financially_innocent") {{ 'selected' }} @endif>{{__('Financially innocent')}}</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="debt" id="financial_liability" value="option2" disabled>
+        <label for="financial_liability" class="form-check-label" value="financial_liability" @if (old('debt') == "financial_liability") {{ 'selected' }} @endif>{{__('It has a previous financial liability')}}</label>
+       </div><br><br>
+     <p>{{__('Mr.: The cashier in the branch, please receive an amount and its amount ')}}<input style="width:150px;"type="text" class="input @error('money_debt')is-danger @enderror"id="money_debt" name="money_debt"value="{{ old('money_debt') }}"class="form-control" placeholder="{{__('Enter debt money')}}" disabled/>{{__(' SYP')}}</p><br>
+{{--  بيان أمين الصندوق--}}
+    <hr><br>
+    <p style="font-weight: bold;">{{__('Treasurer statement: ')}}</p><hr><br>
+    <p>{{__('Amount has been received ')}}<input style="width:150px;"type="text" class="input @error('money_order')is-danger @enderror"id="money_order" name="money_order"value="{{ old('money_order') }}"class="form-control" placeholder="{{__('Enter order money')}}" disabled/>{{__(' SYP')}}</p><br><br>
+{{-- قرار رئيس مجلس الإدارة --}}
+    <hr><br>
+    <p style="font-weight: bold;">{{__("Chairman's decision: ")}}</p><br>
+    <textarea class="form-control" id="reasons" rows="3" disabled></textarea><hr><br><br>
+    @endif
     </div>
 </x-layouts.app>
