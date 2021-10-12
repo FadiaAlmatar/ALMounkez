df68ff07da78922967488e70264321570e3d9cf5
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-10-12 09:42:32 +0300

    route for print

--- app/Http/Controllers/FullOrderController.php
+++ app/Http/Controllers/FullOrderController.php
@@ -268,4 +268,19 @@ class FullOrderController extends Controller
     {
         //
     }
+
+    public function printlocal(Request $request,$id)
+    {
+        // dd($id);
+        $order = FullOrder::findOrFail($id);
+        $html = view('fullorder.local-pdf',['order'=>$order])->render();
+
+        $pdf = new \Mpdf\Mpdf([
+            'mode' => 'utf-8', 'format' => 'A4','default_font' => 'fontawesome','margin_left' => 15,'margin_right' => 10,'margin_top' => 16,'margin_bottom' => 15,'margin_header' => 10, 'margin_footer' => 10 ]);
+        $pdf->AddPage("P");
+        $pdf->SetHTMLFooter('<p style="text-align: center">{PAGENO} of {nbpg}</p>');
+        $pdf->WriteHTML('.fa { font-family: fontawesome;}',1);
+        $pdf->WriteHTML($html);
+        return  $pdf->Output("order.pdf","D");
+        }
 }
--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::get('/locale/en', function (){
     App::setLocale('en');
     return redirect()->back();
 });
+//resource
 Route::resource('orders', OrderController::class);
 Route::resource('posts', PostController::class);
 Route::resource('comments', CommentController::class);
@@ -49,21 +50,24 @@ Route::resource('messages', MessageController::class);
 Route::resource('groups', GroupController::class);
 Route::resource('groups', GroupController::class);
 Route::resource('fullorders', FullOrderController::class);
-
+//comment function
+Route::post('/toggle-approve', [CommentController::class,'approval'])->name('approve');
+//chat functions
 Route::get('/chat/{id}', [MessageController::class, 'chat'])->name('messages.chat');
 Route::get('/chatgroup/{id}', [MessageController::class, 'chatgroup'])->name('messages.chatgroup');
 Route::get('/print/{id}', [MessageController::class, 'print'])->name('messages.print');
 Route::get('/printgroup/{id}', [MessageController::class, 'printgroup'])->name('messages.printgroup');
-Route::post('/toggle-approve', [CommentController::class,'approval'])->name('approve');
 Route::get('/get-friends', [MessageController::class, 'getfriends'])->name('friends');
 Route::get('/new-subscribes/{id}', [GroupController::class, 'addsubscribes'])->name('addsubscribes');
+//order function
 Route::get('/print/{id}', [OrderController::class, 'printorder'])->name('orders.printorder');
-//fullorders
+//fullorders functions
 Route::get('/fullorder/create_local', [FullOrderController::class, 'create_local'])->name('fullorders.create_local');
 Route::get('/fullorder/create_external', [FullOrderController::class, 'create_external'])->name('fullorders.create_external');
 Route::get('/fullorder/create_transfer', [FullOrderController::class, 'create_transfer'])->name('fullorders.create_transfer');
 Route::get('/fullorder/create_replacement', [FullOrderController::class, 'create_replacement'])->name('fullorders.create_replacement');
 Route::post('/fullorder/store_order/{id}', [FullOrderController::class, 'store_order'])->name('fullorders.store_order');
+Route::get('/print/{id}', [FullOrderController::class, 'printlocal'])->name('fullorders.printlocal');
 
 Route::get('/dashboard', function () {
     return view('pages.home');
