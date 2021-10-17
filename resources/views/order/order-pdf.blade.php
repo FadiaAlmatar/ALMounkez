d5f235fdad728f24d61e8fe1ecc869a88576e4d1
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-10-17 14:13:23 +0300

    order pdf

--- resources/views/order/order-pdf.blade.php
+++ resources/views/order/order-pdf.blade.php
@@ -12,9 +12,12 @@
                 width:100%;
                 border: 1px solid black;
                 border-collapse: collapse;
+                height:75%;
+                direction: rtl;
             }
             td,th{
                 border: 1px solid black;
+                height: 15%;
             }
             th{
                 text-align: right;
@@ -37,12 +40,13 @@
             }
             table{
                 width:100%;
+                height:75%;
                 border: 1px solid black;
                 border-collapse: collapse;
             }
             td,th{
                 border: 1px solid black;
-                width:24%;
+                height: 15%;
             }
             td{
                 text-align: center;
@@ -55,17 +59,19 @@
     </head>
     <body>
         {{-- <h5>{{__('Personal data')}} </h5> --}}
+        <p>{{__('Please accept my affiliation with the Syndicate of Financial and Accounting Professions')}}
+            {{__('I declare that all the information, data and attached documents required for affiliation that I have submitted are correct and on my responsibility, and I pledge to abide by the duties of the affiliated members specified in the bylaws of the union, and the relevant decisions and instructions.')}}</p>
           <table class="table table-bordered">
               <tr>
-                  <th scope="col" colspan="6">{{__('Personal data')}}</th>
+                  <th scope="col" colspan="6" style="font-size: 20px;">{{__('Personal data')}}</th>
               </tr>
             <tr>
                 <th scope="col">{{__('FullName/English')}}</th>
-                <td scope="row" colspan="5">{{ $order->english_firstname }}{{ $order->english_father_name }}{{ $order->english_lastname }}</td>
+                <td scope="row" colspan="5">{{ $order->english_firstname }} {{ $order->english_father_name }} {{ $order->english_lastname }}</td>
               </tr>
             <tr>
               <th scope="col">{{__('Name and Nickname')}}</th>
-              <td scope="row">{{ $order->firstname }}{{ $order->lastname}}</td>
+              <td scope="row">{{ $order->firstname }} {{ $order->lastname}}</td>
               <th scope="col">{{__('Father name')}}</th>
               <td scope="row">{{ $order->father_name }}</td>
               <th scope="col">{{__('Grandfather name')}}</th>
@@ -75,13 +81,13 @@
               <th scope="col">{{__('Mother name')}}</th>
               <td scope="row">{{ $order->mother_name }}</td>
               <th scope="col">{{__('Gender')}}</th>
-              <td scope="row">>{{ $order->gender }}</td>
+              <td scope="row">{{ $order->gender }}</td>
               <th scope="col">{{__('Nationality')}}</th>
               <td scope="row">{{$order->Nationality}}</td>
             </tr>
             <tr>
               <th scope="col">{{__('Place and Date of birth')}}</th>
-              <td scope="row">{{ $order->place_of_birth}}{{ $order->date_of_birth}}</td>
+              <td scope="row">{{ $order->place_of_birth}} {{ $order->date_of_birth}}</td>
               <th scope="col">{{__('National ID')}}</th>
               <td scope="row">{{ $order->national_id }}</td>
               <th scope="col">{{__('Personal Identification Number')}}</th>
@@ -106,7 +112,7 @@
     {{-- <h5>{{__('contact information')}}</h5> --}}
         <table class="table table-bordered">
             <tr>
-                <th scope="col" colspan="6">{{__('contact information')}}</th>
+                <th scope="col" colspan="6" style="font-size: 20px;">{{__('contact information')}}</th>
             </tr>
             <tr>
                 <th scope="col">{{__('House Phone')}}</th>
@@ -131,7 +137,7 @@
                         <table class="table" id="Qualification">
                             <thead>
                                 <tr>
-                                    <th scope="col" colspan="6">{{__('Qualifications')}}</th>
+                                    <th scope="col" colspan="6" style="font-size: 20px;">{{__('Qualifications')}}</th>
                                 </tr>
                             <tr>
                                 <th scope="col">{{__('Qualification')}}</th>
@@ -154,25 +160,63 @@
                             </tr>
                             @endfor
                             <tr>
-                                <th scope="col">{{__('How to pay the affiliation fee')}}</th>
-                                <td scope="row" colspan="5">{{__('Payment method')}}</td>
+                                <th scope="row" colspan="6" style="font-size: 20px;">{{__('Attached to the order')}}</th>
+                            </tr>
+                            <tr>
+                                <td scope="row" colspan="3">{{__('Document of no conviction')}}</td>
+                                <td scope="row" colspan="3">{{__('A certified copy of the university degree applied for affiliation with')}}</td>
+                            </tr>
+                            <tr>
+                                <td scope="row" colspan="6">{{__('ID photo A4 size + 3 personal photos')}}</td>
+                            </tr>
+                            <tr style="border: none;">
+                              <td scope="row" rowspan="2" colspan="4">{{__('the employee in charge')}}</td>
+                              <td scope="row" colspan="2">{{__('order presenter')}}</td>
+                            </tr>
+                            <tr style="border: none;">
+                              <td scope="row" colspan="2">{{__('Signature')}}</td>
+                            </tr>
+                            <tr style="border: none;">
+                                <th scope="col" style="font-size: 20px;"colspan="4">{{__('How to pay the affiliation fee')}}</th>
+                                {{-- <td scope="row" colspan="5">{{$order->pay_affiliation_fee}}</td> --}}
+                                <td scope="row" colspan="2" >{{__('Managing Director:')}}</td>
+                            </tr>
+                            <tr style="border: none;">
+                                <td scope="row" colspan="6">{{__('Pay by receipt /bank notice No./         / date')}}</td>
+                            </tr>
+                            <tr style="border: none;">
+                                <td scope="row"  colspan="4" style="border: none;">{{__('cashier:')}}</td>
+                                <td scope="row"  colspan="2" style="border: none;">{{__('Signature')}}</td>
+                            </tr>
+                            <tr style="border: none;">
+                                <td scope="row" colspan="2" style="border: none;">{{__('Signature')}}</td>
+                                <td colspan="4" style="border: none;">{{__('The seal')}}</td>
+                            </tr>
+                            <tr style="border: none;">
+                                <td scope="row" colspan="6" ></td>
+                            </tr>
+                            <tr style="border: none;">
+                                <td scope="row" colspan="6" ></td>
+                            </tr>
+                            <tr>
+                                <th scope="row" colspan="6" style="font-size: 20px;">{{__('Board of directors decision')}}</th>
+                            </tr>
+                            <tr>
+                               <td scope="row" colspan="6">{{__('decision of the Board of Directors in its session No. /      / date   / / 20')}}</td>
+                            </tr>
+                            <tr>
+                               <td scope="row" colspan="2">{{__('Approval')}}</td>
+                               <td scope="row" colspan="4">{{__('Membership No')}}</td>
+                            </tr>
+                            <tr>
+                              <td scope="row" colspan="3">{{__('order is not approved')}}</td>
+                              <td scope="row" colspan="3">{{__('Reasons for disapproval')}}</td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
         </div>
-        {{-- <h5>{{__('How to pay the affiliation fee')}}</h5> --}}
-          {{-- <div id="pay" class="tab-pane fade"  role="tabpanel" aria-labelledby="pay-tab">
-            <div class="row mb-4">
-                <div class="col">
-                  <div class="form-outline">
-                    <label class="form-label" for="Payment method">{{__('Payment method')}}</label>
-                        {{$order->pay_affiliation_fee}}
-                  </div>
-                </div>
-              </div>
-        </div> --}}
-        {{-- نهايةطريقة الدفع --}}
+
     </div>
     </body>
 </html>
