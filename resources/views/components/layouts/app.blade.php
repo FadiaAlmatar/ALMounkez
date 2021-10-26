64e087b9c883ab84b3cf231cd4b1040e47a0f332
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-10-26 15:17:43 +0300

    fix

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -91,7 +91,7 @@
         $('#Qualification').find('tbody').append($('' +
             '<tr class="cloning_row" id="' + numberIncr + '">' +
             '<td><button type="button" class="btn btn-danger btn-sm delegated-btn"><i class="fa fa-minus"></i></button></td>' +
-            '<td><select name="qualification[' + numberIncr + ']" class="form-select form-control"><option></option><option value="Doctorate">{{__('Doctorate')}}</option><option value="Master">{{__('Master')}}</option><option value="Diploma">{{__('Diploma')}}</option><option value="Certificate">{{__('Certificate')}}</option><option value="Other">{{__('Other')}}</option></select></td>' +
+            '<td><select name="qualification[' + numberIncr + ']" class="form-select form-control"><option></option><option value="Doctorate">{{__('Doctorate')}}</option><option value="Master">{{__('Master')}}</option><option value="Diploma">{{__('Diploma')}}</option><option value="Certificate">{{__('Certificate')}}</option><option value="Language">{{__('Language')}}</option><option value="Soft skills">{{__('Soft skills')}}</option><option value="Hard skills">{{__('Hard skills')}}</option><option value="Other">{{__('Other')}}</option></select></td>' +
             '<td><input class="input"type="text" name="specialization['+ numberIncr + ']" class=" form-control"></td>' +
             '<td><input class="input"type="text" name="side[' + numberIncr + ']" class="form-control"></td>' +
             '<td><input class="input"type="text" name="country[' + numberIncr + ']" class="form-control"></td>' +
