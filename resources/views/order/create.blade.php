c4243a761dfddf79969857fa26c2e392facf8156
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-11-01 10:41:20 +0200

    fix lang

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -21,8 +21,6 @@
     @if (!empty($order))
         @method('PUT')
     @endif
-    {{-- <form action="{{ route('orders.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf --}}
         <div class="tab-content" id="nav-tabContent" style="margin-bottom:5px;">
 {{-- البيانات الشخصية --}}
       <div id="Personal"  class="tab-pane fade show active" role="tabpanel" aria-labelledby="Personal-tab">
@@ -84,13 +82,8 @@
                         <option value="{{ $order->gender }}" selected> {{ $order->gender }}</option>
                     @endif
                     <option></option>
-                    @if(app()->getLocale() == 'ar')
-                    <option value="{{__('Male')}}" @if (old('gender') == "male") {{ 'selected' }} @endif>  {{__('Male')}}  </option>
-                    <option value="{{__('Female')}}" @if (old('gender') == "female") {{ 'selected' }} @endif>{{__('Female')}}</option>
-                    @else
                     <option value="male" @if (old('gender') == "male") {{ 'selected' }} @endif>  {{__('Male')}}  </option>
                     <option value="female" @if (old('gender') == "female") {{ 'selected' }} @endif>{{__('Female')}}</option>
-                    @endif
               </select>
               </div>
             </div>
@@ -144,13 +137,8 @@
                          <option value="{{ $order->Nationality}}" selected> {{ $order->Nationality }}</option>
                     @endif
                     <option></option>
-                    @if(app()->getLocale() == 'ar')
-                    <option value="{{__('Syrian')}}" @if (old('Nationality') == "Syrian") {{ 'selected' }} @endif>{{__('Syrian')}}</option>
-                    <option value="{{__('Palestinian')}}" @if (old('Nationality') == "Palestinian") {{ 'selected' }} @endif>{{__('Palestinian')}}</option>
-                    @else
                     <option value="Syrian" @if (old('Nationality') == "Syrian") {{ 'selected' }} @endif>{{__('Syrian')}}</option>
                     <option value="Palestinian" @if (old('Nationality') == "Palestinian") {{ 'selected' }} @endif>{{__('Palestinian')}}</option>
-                    @endif
                 </select>
               </div>
             </div>
@@ -162,17 +150,10 @@
                          <option value="{{ $order->Marital_status }}" selected> {{ $order->Marital_status }}</option>
                     @endif
                     <option></option>
-                    @if(app()->getLocale() == 'en')
                     <option value="Unmarried" @if (old('martialStatus') == "Unmarried") {{ 'selected' }} @endif>{{__('Unmarried')}}</option>
                     <option value="Married" @if (old('martialStatus') == "Married") {{ 'selected' }} @endif>{{__('Married')}}</option>
                     <option value="Divorced" @if (old('martialStatus') == "Divorced") {{ 'selected' }} @endif>{{__('Divorced')}}</option>
                     <option value="Widower" @if (old('martialStatus') == "Widower") {{ 'selected' }} @endif>{{__('Widower')}}</option>
-                    @else
-                    <option value="{{__('Unmarried')}}" @if (old('martialStatus') == "Unmarried") {{ 'selected' }} @endif>{{__('Unmarried')}}</option>
-                    <option value="{{__('Married')}}" @if (old('martialStatus') == "Married") {{ 'selected' }} @endif>{{__('Married')}}</option>
-                    <option value="{{__('Divorced')}}" @if (old('martialStatus') == "Divorced") {{ 'selected' }} @endif>{{__('Divorced')}}</option>
-                    <option value="{{__('Widower')}}" @if (old('martialStatus') == "Widower") {{ 'selected' }} @endif>{{__('Widower')}}</option>
-                    @endif
                  </select>
               </div>
             </div>
@@ -255,15 +236,9 @@
                          <option value="{{ $order->military }}" selected> {{ $order->military }}</option>
                     @endif
                 <option></option>
-                @if(app()->getLocale() == 'ar')
-                <option value="{{__('Finished')}}" @if (old('military') == "Finished") {{ 'selected' }} @endif>{{__('Finished')}}</option>
-                <option value="{{__('Delayed')}}" @if (old('military') == "Delayed") {{ 'selected' }} @endif>{{__('Delayed')}}</option>
-                <option value="{{__('Exempt')}}" @if (old('military') == "Exempt") {{ 'selected' }} @endif>{{__('Exempt')}}</option>
-                @else
                 <option value="Finished" @if (old('military') == "Finished") {{ 'selected' }} @endif>{{__('Finished')}}</option>
                 <option value="Delayed" @if (old('military') == "Delayed") {{ 'selected' }} @endif>{{__('Delayed')}}</option>
                 <option value="Exempt" @if (old('military') == "Exempt") {{ 'selected' }} @endif>{{__('Exempt')}}</option>
-                @endif
              </select>
           </div>
         </div>
@@ -283,13 +258,8 @@
                          <option value="{{ $order->Health_status }}" selected> {{ $order->Health_status }}</option>
                     @endif
                 <option></option>
-                @if(app()->getLocale() == 'ar')
-                <option value="{{__('Good')}}" @if (old('healthStatus') == "Good") {{ 'selected' }} @endif>{{__('Good')}}</option>
-                <option value="{{__('Sick')}}" @if (old('healthStatus') == "Sick") {{ 'selected' }} @endif>{{__('Sick')}}</option>
-                @else
                 <option value="Good" @if (old('healthStatus') == "Good") {{ 'selected' }} @endif>{{__('Good')}}</option>
                 <option value="Sick" @if (old('healthStatus') == "Sick") {{ 'selected' }} @endif>{{__('Sick')}}</option>
-                @endif
              </select>
           </div>
         </div>
@@ -307,20 +277,20 @@
                       <option value="{{ $order->Affiliation_country }}" selected> {{ $order->Affiliation_country}}</option>
                     @endif
                     <option></option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Damascus')}}" @else value="Damascus" @endif    @if (old('countryJoin') == "Damascus") {{ 'selected' }} @endif>       {{__('Damascus')}}      </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Damascus Rural')}}" @else value="Damascus Rural" @endif @if (old('countryJoin') == "Damascus Rural") {{ 'selected' }} @endif> {{__('Damascus Rural')}}</option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Suwayda')}}" @else value="Suwayda" @endif       @if (old('countryJoin') == "Suwayda") {{ 'selected' }} @endif>        {{__('Suwayda')}}       </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Daraa')}}" @else value="Daraa" @endif          @if (old('countryJoin') == "Daraa") {{ 'selected' }} @endif>          {{__('Daraa')}}         </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Quneitra')}}" @else value="Quneitra" @endif      @if (old('countryJoin') == "Quneitra") {{ 'selected' }} @endif>       {{__('Quneitra')}}      </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Homs')}}" @else value="Homs" @endif           @if (old('countryJoin') == "Homs") {{ 'selected' }} @endif>           {{__('Homs')}}          </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Hama')}}" @else value="Hama" @endif          @if (old('countryJoin') == "Hama") {{ 'selected' }} @endif>           {{__('Hama')}}          </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Latakia')}}" @else value="Latakia" @endif       @if (old('countryJoin') == "Latakia") {{ 'selected' }} @endif>        {{__('Latakia')}}       </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Tartous')}}" @else value="Tartous" @endif       @if (old('countryJoin') == "Tartous") {{ 'selected' }} @endif>        {{__('Tartous')}}       </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Idlib')}}" @else value="Idlib" @endif          @if (old('countryJoin') == "Idlib") {{ 'selected' }} @endif>          {{__('Idlib')}}         </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Aleppo')}}" @else value="Aleppo" @endif         @if (old('countryJoin') == "Aleppo") {{ 'selected' }} @endif>         {{__('Aleppo')}}        </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Al-Rakka')}}" @else value="Al-Rakka" @endif       @if (old('countryJoin') == "Al-Rakka") {{ 'selected' }} @endif>       {{__('Al-Rakka')}}      </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Deer Al Zour')}}" @else value="Deer Al Zour" @endif   @if (old('countryJoin') == "Deer Al Zour") {{ 'selected' }} @endif>   {{__('Deer Al Zour')}}  </option>
-                    <option @if(app()->getLocale() == 'ar') value="{{__('Al-Hasakah')}}" @else value="Al-Hasakah" @endif    @if (old('countryJoin') == "Al-Hasakah") {{ 'selected' }} @endif>     {{__('Al-Hasakah')}}    </option>
+                    <option  value="Damascus"    @if (old('countryJoin') == "Damascus") {{ 'selected' }} @endif>       {{__('Damascus')}}      </option>
+                    <option  value="Damascus Rural"  @if (old('countryJoin') == "Damascus Rural") {{ 'selected' }} @endif> {{__('Damascus Rural')}}</option>
+                    <option  value="Suwayda"        @if (old('countryJoin') == "Suwayda") {{ 'selected' }} @endif>        {{__('Suwayda')}}       </option>
+                    <option  value="Daraa"         @if (old('countryJoin') == "Daraa") {{ 'selected' }} @endif>          {{__('Daraa')}}         </option>
+                    <option  value="Quneitra"      @if (old('countryJoin') == "Quneitra") {{ 'selected' }} @endif>       {{__('Quneitra')}}      </option>
+                    <option  value="Homs"          @if (old('countryJoin') == "Homs") {{ 'selected' }} @endif>           {{__('Homs')}}          </option>
+                    <option  value="Hama"         @if (old('countryJoin') == "Hama") {{ 'selected' }} @endif>           {{__('Hama')}}          </option>
+                    <option  value="Latakia"       @if (old('countryJoin') == "Latakia") {{ 'selected' }} @endif>        {{__('Latakia')}}       </option>
+                    <option  value="Tartous"       @if (old('countryJoin') == "Tartous") {{ 'selected' }} @endif>        {{__('Tartous')}}       </option>
+                    <option  value="Idlib"         @if (old('countryJoin') == "Idlib") {{ 'selected' }} @endif>          {{__('Idlib')}}         </option>
+                    <option value="Aleppo"         @if (old('countryJoin') == "Aleppo") {{ 'selected' }} @endif>         {{__('Aleppo')}}        </option>
+                    <option  value="Al-Rakka"       @if (old('countryJoin') == "Al-Rakka") {{ 'selected' }} @endif>       {{__('Al-Rakka')}}      </option>
+                    <option  value="Deer Al Zour"   @if (old('countryJoin') == "Deer Al Zour") {{ 'selected' }} @endif>   {{__('Deer Al Zour')}}  </option>
+                    <option  value="Al-Hasakah"     @if (old('countryJoin') == "Al-Hasakah") {{ 'selected' }} @endif>     {{__('Al-Hasakah')}}    </option>
                  </select>
                 @error('countryJoin')
                     <p class="help is-danger">{{ $message }}</p>
@@ -479,8 +449,6 @@
             <p class="help is-danger">{{ $message }}</p>
             @enderror
           </div>
-          {{-- e0c2c2; --}}
-          {{-- <button type="submit" class="btn btn-primary">{{__('Send')}}</button> --}}
           @if (!empty($order))
           <button type="submit" class="btn btn-primary">{{__('Edit')}}</button><br>
           @else
@@ -550,7 +518,6 @@
                             <td>
                                 <div class="form-group">
                                     {{-- value="{{ old('specialization')[0] ?? "" }}" --}}
-
                                   <input class="input"type="text" name="specialization[0]" value= "@if(!empty($order)) {{$order->qualifications[0]->Specialization}} @else {{ old('specialization')[0] ?? ""}} @endif"  class="form-control" id="Specialization" placeholder="">
                                     @if ($errors->has('specialization.0'))
                                         @foreach($errors->get('specialization.0') as $error)
@@ -575,7 +542,6 @@
                             <td>
                               <div class="form-group">
                                 {{-- value="{{ old('country')[0] ?? "" }}" --}}
-
                                 <input class="input"type="text" name="country[0]" value= "@if(!empty($order)) {{$order->qualifications[0]->country}} @else {{ old('country')[0] ?? ""}} @endif" class="form-control" id="Country" placeholder="">
                                 @if ($errors->has('country.0'))
                                     @foreach($errors->get('country.0') as $error)
@@ -585,7 +551,6 @@
                             </div>
                             </td>
                             <td>
-
                               <div class="form-group">
                                 <input class="input" name="finishYear[0]"  type="text" value= "@if(!empty($order)) {{$order->qualifications[0]->finishyear}} @else {{ old('finishYear')[0] ?? ""}} @endif" class="form-control" id="Year" placeholder="">
                                 @if ($errors->has('finishYear.0'))
@@ -597,7 +562,6 @@
                             </td>
                             <td>
                               <div class="form-group">
-
                                 <input class="input" name="Rate[0]" type="text" value= "@if(!empty($order)) {{$order->qualifications[0]->rate}} @else {{ old('Rate')[0] ?? ""}} @endif" class="form-control" id="Rate" placeholder="">
                                 @if ($errors->has('Rate.0'))
                                     @foreach($errors->get('Rate.0') as $error)
@@ -665,15 +629,9 @@
                     @endif
                     @endif
                     <option></option>
-                    @if(app()->getLocale() == 'ar')
-                    <option value="{{__('Cash to the Central Syndicate Fund in Damascus')}}" @if (old('payment') == "cash") {{ 'selected' }} @endif>{{__('Cash to the Central Syndicate Fund in Damascus')}}</option>
-                    <option value="{{__('Syndicate account with the real estate bank number 11011418 in all countries')}}" @if (old('payment') == "real estate bank") {{ 'selected' }} @endif>{{__('Syndicate account with the real estate bank number 11011418 in all countries')}}</option>
-                    <option value="{{__('Syndicate account with Francbank Bank number 0004204801 in Damascus,Aleppo,Tartous and Latakia')}}" @if (old('payment') == "Francbank") {{ 'selected' }} @endif>{{__('Syndicate account with Francbank Bank number 0004204801 in Damascus,Aleppo,Tartous and Latakia')}}</option>
-                    @else
                     <option value="cash" @if (old('payment') == "cash") {{ 'selected' }} @endif>{{__('Cash to the Central Syndicate Fund in Damascus')}}</option>
                     <option value="real estate bank" @if (old('payment') == "real estate bank") {{ 'selected' }} @endif>{{__('Syndicate account with the real estate bank number 11011418 in all countries')}}</option>
                     <option value="Francbank" @if (old('payment') == "Francbank") {{ 'selected' }} @endif>{{__('Syndicate account with Francbank Bank number 0004204801 in Damascus,Aleppo,Tartous and Latakia')}}</option>
-                    @endif
                 </select>
               </div>
             </div>
