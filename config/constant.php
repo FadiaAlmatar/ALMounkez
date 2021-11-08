e85312eeb7a6d288688ac6becd4b919b1d5ae72f
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-11-08 14:03:32 +0200

    fix

--- config/constant.php
+++ config/constant.php
@@ -2,6 +2,6 @@
 
 return [
     'countries'=>['Damascus','Damascus Rural','Suwayda','Daraa','Quneitra','Homs','Hama','Latakia','Tartous', 'Idlib','Aleppo','Al-Rakka','Deer Al Zour','Al-Hasakah'],
-     'status' =>['Payment required','Please pick up', 'not confirmed','Need to complete papers','Finished']
+     'statuses' =>['Payment required','Please pick up', 'not confirmed','Need to complete papers','Finished']
 
 ];
--- resources/views/fullorder/show_replacement.blade.php
+++ resources/views/fullorder/show_replacement.blade.php
@@ -152,7 +152,7 @@
         <label>{{__('order status')}}</label>
         <select name="status"class="input @error('status')is-danger @enderror" class="form-select" aria-label="Default select example" @if(Auth::User()->role == "user"){{ 'disabled' }} @endif>
             <option selected></option>
-            @foreach (Config::get('constant.status') as $status)
+            @foreach (Config::get('constant.statuses') as $status)
             <option  value={{$status}}  @if (old('status') == $status) {{ 'selected' }} @endif>{{__($status)}}</option>
             @endforeach
         </select>
